38458fdfbe753974b73b46bd639c59096b675826
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2021-12-21 15:58:00 +0300

    Change view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  var today = new Date()
+   const datetoday = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello From React</h1>
+      <p>Created on: <strong>{datetoday}</strong></p>
     </div>
   );
 }
