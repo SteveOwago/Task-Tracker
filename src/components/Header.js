6be5fb0811dea2ac9bbef0f1adfcffff9bd14c72
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2022-01-02 14:24:44 +0300

    Introduction to props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-const Header = () => {
+const Header = ({title,message}) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{title}</h1>
+            <p>{message}</p>
         </header>
     )
+}
+// Destructuring props into object models
+Header.defaultProps = {
+    title: 'Task Tracker',
+    message: 'Introduction to props in React. props are like tag attributes but in react they can be passed to different sections of the application'
 }
 
 export default Header
