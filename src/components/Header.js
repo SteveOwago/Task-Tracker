c9a6ba2883dc3748f2af0d43a5ecdd766faf55f9
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2022-01-02 14:34:07 +0300

    Adding proptypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const Header = ({title,message}) => {
     return (
         <header>
@@ -9,7 +11,12 @@ const Header = ({title,message}) => {
 // Destructuring props into object models
 Header.defaultProps = {
     title: 'Task Tracker',
-    message: 'Introduction to props in React. props are like tag attributes but in react they can be passed to different sections of the application'
+    message: 'Introduction to props in React. props are like tag attributes but in react they can be passed to different sections of the application. Proptypes are used to define the value type of each prop e.g String, number or boolean'
+}
+// Adding proptypes into react props. Helps in defining prop properties and other validation rules.
+Header.propTypes = {
+    title: PropTypes.string,
+    message: PropTypes.string,
 }
 
 export default Header
