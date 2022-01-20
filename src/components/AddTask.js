45d9cd78c78b003e55f354aca5efa4c390f56eb5
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2022-01-20 10:57:00 +0300

    Changing input values using state

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -7,15 +7,15 @@ const AddTask = () => {
         <form action="" className="add-form">
             <div className="form-control">
                 <label htmlFor="">Task</label>
-                <input type="text" placeholder="Add Task"/>
+                <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)}/>
             </div>
             <div className="form-control">
                 <label htmlFor="">Day & Time</label>
-                <input type="text" placeholder="Add Day & Time"/>
+                <input type="text" placeholder="Add Day & Time" value={day} onChange={(e) => setDay(e.target.value)}/>
             </div>
             <div className="form-control form-control-check">
                 <label htmlFor="">Set Reminder</label>
-                <input type="checkbox" />
+                <input type="checkbox" value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)} />
             </div>
             <input type="submit" className="btn btn-block" value="Save Task" />
         </form>
